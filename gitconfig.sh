e668a7e4c434787d30ca86564b66000be8f7ee4a
Soo Philip Kim <philipjkim@gmail.com>
2012-03-29 11:35:55 +0900

    first commit

---
+++ gitconfig.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git config --global user.name "Soo Philip Kim"
+git config --global user.email philipjkim@gmail.com
+
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
